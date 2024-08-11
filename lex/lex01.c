67ed11d786ce43ca6390af4f40e0000e520e0639
nemokunx22 <nemokun.x22@gmail.com>
2024-08-11 07:40:02 +0530

    Update lex01.c
    CHANGES MADE FROM HOME SYSTEM

--- lex/lex01.c
+++ lex/lex01.c
@@ -1,14 +1,13 @@
 //Akash V V
 //Roll no : 09
-#include<stdio.h>
-#include<ctype.h>
-#include<string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 int main() {
-    int l = 1;
-    int t = 0;
-    int j = 0;
-    int i, flag;
+    int l = 1; // Line number
+    int t = 0; // Token number
+    int i, j, flag;
     char ch, str[20];
     FILE *input;
 
@@ -26,41 +25,79 @@ int main() {
 
     printf("Line no. \t Token no. \t\t Token \t\t Lexeme\n\n");
 
-    while (!feof(input)) {
-        i = 0;
-        flag = 0;
-        ch = fgetc(input);
+    while ((ch = fgetc(input)) != EOF) {
+        // Check for single-line comments
+        if (ch == '/') {
+            char nextChar = fgetc(input);
+            if (nextChar == '/') {
+                // Skip the rest of the line
+                while (ch != '\n' && ch != EOF) {
+                    ch = fgetc(input);
+                }
+                if (ch == '\n') {
+                    l++; // Update line number for the next line
+                }
+                continue;
+            } else if (nextChar == '*') {
+                // Skip multi-line comments
+                ch = fgetc(input); // Continue reading after '/*'
+                while (ch != EOF) {
+                    if (ch == '*') {
+                        char afterStar = fgetc(input);
+                        if (afterStar == '/') {
+                            break; // End of multi-line comment
+                        } else {
+                            ungetc(afterStar, input);
+                        }
+                    }
+                    ch = fgetc(input);
+                }
+                continue;
+            } else {
+                ungetc(nextChar, input); // Not a comment, put the character back
+            }
+        }
 
+        // Process tokens
         if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
             printf("%7d\t\t %7d\t\t Operator\t %7c\n", l, t, ch);
             t++;
-        }
-
-        else if (ch == ';' || ch == '{' || ch == '}' || ch == '(' || ch == ')' || 
-                 ch == '?' || ch == '@' || ch == '!' || ch == '%') {
+        } else if (ch == ';' || ch == '{' || ch == '}' || ch == '(' || ch == ')' || 
+                   ch == '?' || ch == '@' || ch == '!' || ch == '%') {
             printf("%7d\t\t %7d\t\t Special symbol\t %7c\n", l, t, ch);
             t++;
-        }
-
-        else if (isdigit(ch)) {
-            printf("%7d\t\t %7d\t\t Digit\t\t %7c\n", l, t, ch);
-            t++;
-        }
-
-        else if (isalpha(ch)) {
-            str[i] = ch;
-            i++;
+        } else if (isdigit(ch)) {
+            // Process digits
+            str[0] = ch;
+            i = 1;
             ch = fgetc(input);
-
-            while (isalnum(ch) && ch != ' ') {
+            while (isdigit(ch)) {
                 str[i] = ch;
                 i++;
                 ch = fgetc(input);
             }
-
             str[i] = '\0';
+            ungetc(ch, input); // Put the last read character back to process later
 
-            for (j = 0; j <= 30; j++) {
+            printf("%7d\t\t %7d\t\t Digit\t\t %7s\n", l, t, str);
+            t++;
+        } else if (isalpha(ch)) {
+            // Process identifiers and keywords
+            str[0] = ch;
+            i = 1;
+            ch = fgetc(input);
+
+            while (isalnum(ch) || ch == '_') {
+                str[i] = ch;
+                i++;
+                ch = fgetc(input);
+            }
+            str[i] = '\0';
+            ungetc(ch, input); // Put the last read character back to process later
+
+            // Check if the identifier is a keyword
+            flag = 0;
+            for (j = 0; j < 32; j++) {
                 if (strcmp(str, keyword[j]) == 0) {
                     flag = 1;
                     break;
@@ -69,14 +106,11 @@ int main() {
 
             if (flag == 1) {
                 printf("%7d\t\t %7d\t\t Keyword\t %7s\n", l, t, str);
-                t++;
             } else {
                 printf("%7d\t\t %7d\t\t Identifier\t %7s\n", l, t, str);
-                t++;
             }
-        }
-
-        else if (ch == '\n') {
+            t++;
+        } else if (ch == '\n') {
             l++;
         }
     }
@@ -84,4 +118,3 @@ int main() {
     fclose(input);
     return 0;
 }
-
